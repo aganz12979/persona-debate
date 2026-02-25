e7872bb3588b55403911f45714ee6de8d5e97d66
aganz12979 <aganz1297@gmail.com>
2026-02-24 19:27:41 -0800

    Demo only

---
+++ config-keys.js
@@ -0,0 +1,5 @@
+// This file contains your API key
+// It is gitignored and will NOT be pushed to GitHub
+const CONFIG_KEYS = {
+  OPENAI_API_KEY: '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'
+};
